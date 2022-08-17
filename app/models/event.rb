538ca42c05d1336ad8772d6aeb15d95cd74bbb69
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-08-17 12:28:47 +0300

    added styles, some fixes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
+  validates :user, presence: true
+
+  belongs_to :user
 end
