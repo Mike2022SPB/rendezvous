83374f69499215a1eaebb3ae00751aa953234508
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-08-19 13:21:04 +0300

    added comments controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
 
   root to: "events#index"
 
-  resources :events
+  resources :events do
+    resources :comments, only: %i[create destroy]
+  end
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: %i[show edit update]
 end
