42c3efeccd1b71f815844711be071b992cec5801
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-08-29 18:59:34 +0300

    added photo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,30 @@
 module ApplicationHelper
 
   def user_avatar(user)
-    if @user.avatar?
-      @user.avatar.url
+    if user.avatar?
+      user.avatar.url
     else
       asset_path('user.png')
+    end
+  end
+
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_path('event.jpg')
+    end
+  end
+
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_path('event_thumb.jpg')
     end
   end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -7,8 +7,7 @@ class Event < ApplicationRecord
   belongs_to :user
 
   has_many :comments, dependent: :destroy
-
   has_many :subscriptions, dependent: :destroy
-
   has_many :subscribers, through: :subscriptions, source: :user
+  has_many :photos
 end
