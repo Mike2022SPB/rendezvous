61db3a6a4d8ee7422a2a78c989ec19f05e505b1c
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-09-02 14:45:51 +0300

    intermediate commit

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,7 @@ class Image < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  has_one_attached :image
+  has_one_attached :image do |attacheble|
+    attacheble.variant :thumb, resize_to_limit: [300, 300]
+  end
 end
