5de10bf9e055ca86a21f3d726c06de69d7269474
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-09-06 10:05:10 +0300

    added validation to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,8 @@ class Image < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image do |attacheble|
-    attacheble.variant :thumb, resize_to_limit: [300, 300]
+    attacheble.variant :thumb,
+      resize_to_limit: [300, 300],
+      content_type: %i[png jpg jpeg]
   end
 end
